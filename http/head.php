09932db9e0371f3dadcad1e38b663573979c7180
voxdemonix <voxdemonix@users.noreply.github.com>
2014-09-26 00:08:22 +0200

    Update head.php

--- http/head.php
+++ http/head.php
@@ -1,3 +1,4 @@
+<?php include_once('./skin.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,11 +7,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <meta http-equiv="refresh" content="600;URL='/'" />
-
-  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <meta http-equiv="refresh" content="600">
+<?php echo skinDisplay(); //skin.php
+/*  <link href="/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/bootstrap-minepeon.css" rel="stylesheet">
-
+*/	?>
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
   <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
