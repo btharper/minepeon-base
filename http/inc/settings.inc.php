c53a72928557469ccaff3dce36db4aeaff46e29b
voxdemonix <voxdemonix@users.noreply.github.com>
2014-10-28 19:16:42 +0100

    Update settings.inc.php

--- http/inc/settings.inc.php
+++ http/inc/settings.inc.php
@@ -30,19 +30,5 @@ $plea = '
 <h3>Plea</h3>
 <p>Please reconsider your decision to give back absolutely nothing to the project that is currently running your miners. A lot of time and effort has gone into making MinePeon what it is today and a small token of 15 minutes of your hash power would be greatly appreciated and will continue to fund the ongoing development and support of MinePeon.  </p>
 <p>It is such a small amount and well below the normal variance in bitcoin mining you will not even notice the difference. If you work it out for every 1 GH/s you have it is 0.00027 bitcoin a day, ask yourself, is that really too much to support MinePeon?</p>
-<p>Some of the features that I would like to include are;-</p>
-<ul>
-<li>TFT Display</li>
-<li>LCD Display</li>
-<li>Android app</li> 
-<li>iOS app</li>
-<li>Live Update</li>
-<li>SMS/Email Alerts</li>
-<li>Backup/Restore</li>
-<li>Cloud Control</li>
-<li>VPN Tunneling (DDOS Protection & Anonymity)</li>
-</ul>
-<p>Most of those new features cost money to setup and run, I would prefer not to have to make features available as "paid for" addons but it all depends on you.</p>
-<p>Neil Fincham</p>
-<p>The MineForeman</p>';
+<p>Some of the features that I would like to include are;-</p>';
 ?>
