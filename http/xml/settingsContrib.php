f7c4a62bc64691f0bfc42ace7f48dcdcf44558af
voxdemonix <voxdemonix@users.noreply.github.com>
2014-10-28 20:04:51 +0100

    New XML file for control who support

---
+++ http/xml/settingsContrib.php
@@ -0,0 +1,29 @@
+<?xml version="1.0"?>
+<xml>
+#contrib settings
+#This settings allow people can really support the contributor + eventually other fun people (ex: quadrature du net )
+<contribList>
+  <contributor>Mineforeman</contributor>
+  <contributor>MarioGrip</contributor>
+  <contributor>VoxDemonix</contributor>
+</contribList>
+<contribPools>
+  <Mineforeman>stratum+tcp://stratum.btcguild.com:3333</Mineforeman>
+  <MarioGrip>stratum+tcp://uk1.ghash.io:3333</MarioGrip>
+  <VoxDemonix>stratum+tcp://eu.multipool.us:9332</VoxDemonix>
+</contribPools>
+<contribWorker>
+  <Mineforeman>MinePeon_Donate</Mineforeman>
+  <MarioGrip>mariogrip.donate</MarioGrip>
+  <VoxDemonix>cannard.Donate</VoxDemonix>
+</contribWorker>
+<contribWorkerPass>
+#if 0 = no password
+  <Mineforeman>MinePeon_Donate</Mineforeman>
+  <MarioGrip>0</MarioGrip>
+  <VoxDemonix>0</VoxDemonix>
+</contribWorkerPass>
+<contribLastSupport>Mineforeman</contribLastSupport>
+#Default : 0 ; if contribForceSupport = 1 { don't refresh contribLastSupport and use it }else{ use the next guy who follow contribLastSupport and refresh contribLastSupport }
+<contribForceSupport>0</contribForceSupport>
+</xml>
