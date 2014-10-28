8cffa4a243b8a91f6806633f4dafe30b617a0470
voxdemonix <voxdemonix@users.noreply.github.com>
2014-10-28 17:24:26 +0100

    Add skin voxedition to update's page.

--- http/update.php
+++ http/update.php
@@ -10,8 +10,9 @@ login_check("quick");
 <html>
   <head>
     <title>Updating System</title>
-  <link href="/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/css/bootstrap-minepeon.css" rel="stylesheet">
+    <?php include_once('./skin.php'); echo skinDisplay(); ?>
+<!--  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/bootstrap-minepeon.css" rel="stylesheet"> -->
         <style>
    html, body {
             height: 100%;
