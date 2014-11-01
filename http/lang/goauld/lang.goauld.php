44e4279bc06b60f114e9a5ea693eb6f4e25c7819
voxdemonix <voxdemonix@users.noreply.github.com>
2014-11-01 23:02:26 +0100

    Create lang.goauld.php

---
+++ http/lang/goauld/lang.goauld.php
@@ -0,0 +1,89 @@
+<?php
+//French
+
+//Index
+
+$lang["MPtemp"] = "MinePeon Temp";
+$lang["MPcpu"]  = "MinePeon CPU Charge";
+$lang["bestshare"] = "Meilleur Partage";
+$lang["MPuptime"] = "MinePeon Uptime";
+$lang["mineruptime"] = "Miner Uptime";
+$lang["MPversion"] = "MinePeon Version";
+$lang["minerversion"] = "Miner Version";
+$lang["donationmin"] = "Donation Minutes";
+$lang["restartminer"] = "Restart Miner";
+$lang["reboot"] = "Reboot";
+$lang["shutdown"] = "Éteindre";
+$lang["url"] = "URL";
+$lang["user"] = "Nis'Tra";;
+$lang["last"] = "Dernier";
+$lang["best"] = "Meilleur";
+$lang["divices"] = "Peripheriques";
+$lang["name"] = "Sayku";
+$lang["id"] = "ID";
+$lang["temp"] = "Temp";
+$lang['totals'] = "Totals";
+$lang["nodevicesrunning"] = "Aucun peripherique ne fonctionne";
+$lang["devices"]  = "Peripheriques";
+$lang["servertime"] = "MinePeon Time ";
+$lang["version"] = "MinePeon Version ";
+
+
+//Pools
+
+$lang["warning"] = "WARNING:";
+$lang["warningtext"] = "Il y a une très petite validation pour ces options actuellement, vérifié que vos options sont correctes.";
+$lang["info"] = "Minepeon utilise les pools suivant. Changez les dans votre compte de minage où enlevez les pour passer en mode donation.";
+$lang["username"] = "Username";
+$lang["password"] = "Password";
+$lang["optional"] = "optionel";
+$lang["info2"] = "Après avoir sauvé, le miner va redémarrer avec la nouvelle configuration. Cela peut prendre 10 seconds.";
+$lang["submit"] = "Envoyer";
+
+
+//Settings
+
+$lang["timezone"] = "TimeZone";
+$lang["save"] = "Evel'Moa";
+$lang["newpassword"] = "Nouveau Password";
+$lang["repepassword"] = "Repetez Password";
+$lang["mining"] = "Taparief";
+$lang["expecteddevices"] = "Expected Devices";
+$lang["expectedhashrate"] = "Expected Hashrate";
+$lang["infoed"] = "Si le nombre de peripheriques actifs passe en dessous de cette valeur, une alerte sera envoyée.";
+$lang["infoeh"] = "Si le hashrate passe en dessous de cette valeur, une alerte sera envoyée.";
+$lang["alerts"] = "Alertes";
+
+
+//Plugins
+
+
+$lang["name"] = "Nom";
+$lang["madeby"] = "Fait par";
+$lang["description"] = "Description";
+$lang["delete"] = "Supprimer";
+$lang["instalpl"] = "Installer un plugin";
+$lang["instal"] = "Installation";
+$lang["instalfromweb"] = "Installer un plugin depuis le web";
+$lang["downloadurl"] = "Url de telechargement";
+
+//About
+
+//Contact
+
+//license
+
+//Foot
+
+//Menu
+
+$lang["status"] = "Status";
+$lang["pools"] = "A'Roush";
+$lang["settings"] = "Options";
+$lang["plugins"] = "Plugins";
+$lang["about"] = "About";
+$lang["contact"] = "Contacte";
+$lang["license"] = "License";
+
+
+?>
