565d22b0e68f84f6acf7248530b650b8f6a23276
voxdemonix <voxdemonix@users.noreply.github.com>
2014-11-01 13:08:18 +0100

    Create license.fr.php

---
+++ http/lang/fr/license.fr.php
@@ -0,0 +1,21 @@
+				<p>There are many different components to MinePeon and you will find that they all have some sort of license, please respect their intellectual property rights by complying with their license, you will find the licence details included in the upstream package of the software.</p>
+
+				<p>Le logiciel MinePeon est sous licence GPLv3 (logiciel libre).</p>
+<pre>
+  Les droits garantis:
+   Les termes de la GPL autorisent toute personne à recevoir une copie d'un travail sous GPL. Chaque personne qui adhère aux termes et aux conditions de la GPL a la permission de modifier le travail, de l'étudier et de redistribuer le travail ou un travail dérivé. Cette personne peut toucher de l'argent pour ce service ou bien ne rien toucher. Ce dernier point distingue la GPL des autres licences de logiciels qui interdisent la redistribution dans un but commercial. Stallman pense que le logiciel libre ne devrait pas placer de restriction sur l'utilisation commerciale, et la GPL indique explicitement qu'un travail sous GPL peut être (re)vendu. En cas de modification, le résultat doit être placé sous la même licence.
+   
+   Le copyleft:
+   La GPL ne donne pas à l'utilisateur des droits de redistribution sans limite. Le droit de redistribuer est garanti seulement si l'utilisateur fournit le code source de la version modifiée. En outre, les copies distribuées, incluant les modifications, doivent être aussi sous les termes de la GPL.
+
+Cette condition est connue sous le nom de copyleft, et il obtient son origine légale du fait que le programme est « copyrighté ». Puisqu'il est copyrighté, l'utilisateur n'a aucun droit de le modifier ou de le redistribuer, sauf sous les termes du copyleft. On est obligé d'adhérer à la GPL si on souhaite exercer des droits normalement limités (voire interdits) par le copyright, comme la redistribution. Ainsi, si on distribue des copies du travail sans respecter les termes de la GPL (en gardant le code source secret par exemple), on peut être poursuivi par l'auteur original en vertu du copyright.
+
+Le copyleft emploie ainsi le copyright pour accomplir l'opposé de son but habituel : au lieu des restrictions imposées, il accorde des droits d'utilisation. C'est pour cette raison que la GPL est décrite comme un détournement du copyright. Elle assure également qu'on n'accorde pas de droits illimités de redistribution par l'intermédiaire de n'importe quel bogue légal trouvé dans les termes du copyleft.
+
+Beaucoup de distributeurs de programmes sous GPL fournissent le code source avec l'exécutable. Pour s'acquitter des obligations du copyleft, une autre possibilité est de fournir sur demande le code source sur un support physique, par exemple un cédérom. Dans la pratique, beaucoup de programmes sous GPL sont distribués sur Internet et leur code source est disponible sur FTP, CVS, Git… Cette distribution par Internet étant compatible avec la licence GPL.
+
+Le copyleft s'applique uniquement quand une personne veut redistribuer le programme. On est autorisé à faire des versions modifiées privées, sans aucune obligation de divulguer les modifications effectuées sur le programme s'il n'est distribué à personne. Le copyleft s'applique uniquement au programme et non à ses sorties. Par exemple, un portail Web utilisant une version modifiée privée d'un CMS sous GPL ne sera pas obligé de livrer ses sources. Cette situation est corrigée par l’Affero General Public License, une version de la GPL 3 qui ajoute un paragraphe pour accorder aux utilisateurs d’un programme accédé par un réseau les mêmes droits que les utilisateurs d’un programme installé localement.
+
+    Vous pouvezz trouver une copie de la GNU General Public License
+    autour de ce progrmame. Si non, regardez http://www.gnu.org/licenses/ or https://fr.wikipedia.org/wiki/Licence_publique_g%C3%A9n%C3%A9rale_GNU.
+</pre>
