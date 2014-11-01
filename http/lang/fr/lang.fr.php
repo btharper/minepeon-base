35c66abd7fbe878374114da1c946f636305ef31a
voxdemonix <voxdemonix@users.noreply.github.com>
2014-11-01 13:22:02 +0100

    Update lang.fr.php

--- http/lang/fr/lang.fr.php
+++ http/lang/fr/lang.fr.php
@@ -5,8 +5,8 @@
 //Index
 
 $lang["MPtemp"] = "MinePeon Temp";
-$lang["MPcpu"]  = "MinePeon CPU Load";
-$lang["bestshare"] = "Best Share";
+$lang["MPcpu"]  = "MinePeon CPU Charge";
+$lang["bestshare"] = "Meilleur Partage";
 $lang["MPuptime"] = "MinePeon Uptime";
 $lang["mineruptime"] = "Miner Uptime";
 $lang["MPversion"] = "MinePeon Version";
@@ -14,18 +14,18 @@ $lang["minerversion"] = "Miner Version";
 $lang["donationmin"] = "Donation Minutes";
 $lang["restartminer"] = "Restart Miner";
 $lang["reboot"] = "Reboot";
-$lang["shutdown"] = "ShutDown";
+$lang["shutdown"] = "Éteindre";
 $lang["url"] = "URL";
 $lang["user"] = "User";;
-$lang["last"] = "Last";
-$lang["best"] = "Best";
-$lang["divices"] = "Devices";
-$lang["name"] = "Name";
+$lang["last"] = "Dernier";
+$lang["best"] = "Meilleur";
+$lang["divices"] = "Peripheriques";
+$lang["name"] = "Nom";
 $lang["id"] = "ID";
 $lang["temp"] = "Temp";
 $lang['totals'] = "Totals";
-$lang["nodevicesrunning"] = "No devices running";
-$lang["devices"]  = "Devices";
+$lang["nodevicesrunning"] = "Aucun peripherique ne fonctionne";
+$lang["devices"]  = "Peripheriques";
 $lang["servertime"] = "MinePeon Time ";
 $lang["version"] = "MinePeon Version ";
 
@@ -33,40 +33,40 @@ $lang["version"] = "MinePeon Version ";
 //Pools
 
 $lang["warning"] = "WARNING:";
-$lang["warningtext"] = "There is very little validation on these settings at the moment so make sure your settings are correct!";
-$lang["info"] = "MinePeon will use the following pools. Change it to your mining accounts or leave it to donate.";
+$lang["warningtext"] = "Il y a une très petite validation pour ces options actuellement, vérifié que vos options sont correctes.";
+$lang["info"] = "Minepeon utilise les pools suivant. Changez les dans votre compte de minage où enlevez les pour passer en mode donation.";
 $lang["username"] = "Username";
 $lang["password"] = "Password";
-$lang["optional"] = "optional";
-$lang["info2"] = "After saving, the miner will restart with the new configuration. This takes about 10 seconds.";
-$lang["submit"] = "Submit";
+$lang["optional"] = "optionel";
+$lang["info2"] = "Après avoir sauvé, le miner va redémarrer avec la nouvelle configuration. Cela peut prendre 10 seconds.";
+$lang["submit"] = "Envoyer";
 
 
 //Settings
 
 $lang["timezone"] = "TimeZone";
 $lang["save"] = "Save";
-$lang["newpassword"] = "New Password";
-$lang["repepassword"] = "Repeat Password";
+$lang["newpassword"] = "Nouveau Password";
+$lang["repepassword"] = "Repetez Password";
 $lang["mining"] = "Mining";
 $lang["expecteddevices"] = "Expected Devices";
 $lang["expectedhashrate"] = "Expected Hashrate";
-$lang["infoed"] = "If the count of active devices falls below this value, an alert will be sent.";
-$lang["infoeh"] = "If the hashrate falls below this value an alert will be sent.";
-$lang["alerts"] = "Alerts";
+$lang["infoed"] = "Si le nombre de peripheriques actifs passe en dessous de cette valeur, une alerte sera envoyée.";
+$lang["infoeh"] = "Si le hashrate passe en dessous de cette valeur, une alerte sera envoyée.";
+$lang["alerts"] = "Alertes";
 
 
 //Plugins
 
 
-$lang["name"] = "Name";
-$lang["madeby"] = "Made by";
+$lang["name"] = "Nom";
+$lang["madeby"] = "Fait par";
 $lang["description"] = "Description";
-$lang["delete"] = "Delete";
-$lang["instalpl"] = "Instal plugin";
-$lang["instal"] = "Instal";
-$lang["instalfromweb"] = "Instal plugin from web";
-$lang["downloadurl"] = "Download url";
+$lang["delete"] = "Supprimer";
+$lang["instalpl"] = "Installer un plugin";
+$lang["instal"] = "Installation";
+$lang["instalfromweb"] = "Installer un plugin depuis le web";
+$lang["downloadurl"] = "Url de telechargement";
 
 //About
 
@@ -80,10 +80,10 @@ $lang["downloadurl"] = "Download url";
 
 $lang["status"] = "Status";
 $lang["pools"] = "Pools";
-$lang["settings"] = "Settings";
+$lang["settings"] = "Options";
 $lang["plugins"] = "Plugins";
 $lang["about"] = "About";
-$lang["contact"] = "Contact";
+$lang["contact"] = "Contacte";
 $lang["license"] = "License";
 
 
