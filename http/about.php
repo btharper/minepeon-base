e7628c886386adee331b14fa69199c14ad066a57
voxdemonix <cervox669@gmail.com>
2014-11-02 19:57:16 +0100

    modifié:         http/about.php 	nouveau fichier: http/img/MinePeonQR.png 	nouveau fichier: http/img/facebook.png 	nouveau fichier: http/img/linkedin.png 	nouveau fichier: http/img/twitter.png

--- http/about.php
+++ http/about.php
@@ -9,7 +9,7 @@ include('menu.php');
     <fieldset>
       <legend>About</legend>
                 
-				<img align="right" title="MinePeonQR" alt="Donate to 12Ui8w9q6eq6TxZmow8H9VHbZdfEnsLDsB" src="https://mineforeman.com/wp-content/uploads/2013/01/MinePeonQR.png" width="200" height="200" />
+				<img align="right" title="MinePeonQR" alt="Donate to 12Ui8w9q6eq6TxZmow8H9VHbZdfEnsLDsB" src="img/MinePeonQR.png" width="200" height="200" />
 				<blockquote class="a">Peon's in feudal times were indentured vassals, often assigned dangerous, tedious and unpleasant work, like mining.<cite>MineForeman</cite></blockquote>
         </div>
 
@@ -18,11 +18,11 @@ include('menu.php');
       <legend>Contact</legend>
               
 				<div style="padding: 10px 0 0 0;">You can contact Neil directly at;-</div>
-<div style="padding: 10px 0 0 0;"><img style="border: 0; float: left;" alt="" src="https://mineforeman.com/email/MineFormanEmail.png" />
+<div style="padding: 10px 0 0 0;"><img style="border: 0; float: left;" alt="" src="img/MineFormanEmail.png" />
 <div style="margin-left: 10px; float: left;">
-<div style="padding: 0 0 3px 0;"><a href="https://www.facebook.com/MineForeman"><img style="border: 0;" alt="Facebook" src="https://mineforeman.com/email/facebook.png" /></a></div>
-<div style="padding: 0 0 3px 0;"><a href="https://twitter.com/mineforeman"><img style="border: 0;" alt="Twitter" src="https://mineforeman.com/email/twitter.png" /></a></div>
-<div style="padding: 0 0 3px 0;"><a href="http://www.linkedin.com/company/mineforeman-com"><img style="border: 0;" alt="LinkedIn" src="https://mineforeman.com/email/linkedin.png" /></a></div>
+<div style="padding: 0 0 3px 0;"><a href="https://www.facebook.com/MineForeman"><img style="border: 0;" alt="Facebook" src="img/facebook.png" /></a></div>
+<div style="padding: 0 0 3px 0;"><a href="https://twitter.com/mineforeman"><img style="border: 0;" alt="Twitter" src="img/twitter.png" /></a></div>
+<div style="padding: 0 0 3px 0;"><a href="http://www.linkedin.com/company/mineforeman-com"><img style="border: 0;" alt="LinkedIn" src="img/linkedin.png" /></a></div>
 </div>
 <div style="margin-left: 10px; float: left;">
 <div style="font-weight: bold; font-family: helvetica; font-size: 16px;">Neil Fincham</div>
