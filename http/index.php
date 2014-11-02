d014124b0f27b0d6de59d64cf44d8025b21bafff
voxdemonix <cervox669@gmail.com>
2014-11-02 20:15:44 +0100

    modifié:         http/index.php

--- http/index.php
+++ http/index.php
@@ -191,7 +191,7 @@ document.title = '<?php echo $G_MHSav; ?>|<?php echo $version; ?>';
 // Change screen colour test for alerts
  
 if ($settings['donateAmount'] < 1) {
-	echo 'document.body.style.background = "#FFFFCF"';
+//	echo 'document.body.style.background = "#FFFFCF"';
 }
 
 ?>
