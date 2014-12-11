9d1dd0b76ca11505b8731b4953383843650db2ff
voxdemonix <voxdemonix@users.noreply.github.com>
2014-12-11 14:25:19 +0100

    Update repairRRD.sh

--- bin/scripts/repairRRD.sh
+++ bin/scripts/repairRRD.sh
@@ -2,4 +2,5 @@
 #Re-Install the rrd mod for php
 /usr/bin/sudo pecl uninstall rrd
 /usr/bin/sudo pecl install rrd
+/usr/bin/sudo chmod 555 -R /opt/minepeon/etc/cron.d/5min/RECORDHashrate
 read -p "Press enter key to continue, please, reboot for take effect"
