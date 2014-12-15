bb749899eac728371f839ac4dd13192b02f34790
btharper <btharper1221@gmail.com>
2014-12-15 17:48:06 +0000

    Add minepeon cron directory to fixperms.sh

--- bin/scripts/fixperms.sh
+++ bin/scripts/fixperms.sh
@@ -10,3 +10,4 @@ sudo chown minepeon.minepeon /etc/php/conf.d/TZ.ini
 
 #Permissions
 sudo chmod 700 /opt/minepeon/bin/scripts/*
+sudo find /opt/minepeon/etc/cron.d/ -type f -not -iname README.md -exec sudo chmod 700 {} +
